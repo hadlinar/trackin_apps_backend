391236b5491abe2d18a75e54654842490a52fbb0
hadlinar <hadlinar@gmail.com>
2022-12-16 10:09:16 +0700

    change port

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(logoutRouter);
 
 // app.listen(5000, () => console.log('Server running at http://localhost:5000'))
 
-const port = 3000
+const port = 3001
 
 app.use(function(req, res, next) {
     res.status(404);
